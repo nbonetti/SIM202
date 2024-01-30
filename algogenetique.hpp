12054f7d64b726f3bc0a0da6d8c27b3aedd94bd2
agathe <agathe.pascal@ensta-paris.fr>
2024-01-30 11:53:33 +0100

    ajout du crossover à tester

--- algogenetique.hpp
+++ algogenetique.hpp
@@ -2,7 +2,17 @@
 #include <fstream>
 #include <vector>
 #include <utility>
+#include <random>
 using namespace std;
+
+// fonction qui renvoie un nombre aléatoire entre 1 et n-1 de manière unifrome
+int unif_rand(int n)
+{
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(1, n - 1);
+    return dis(gen);
+}
 
 class individu
 {
@@ -18,6 +28,40 @@ public:
 
     individu crossover(const individu &partner)
     {
+        // Vérifie que les deux parents ont la même taille
+        if (n != partner.n)
+        {
+            cout << "Erreur : Les parents ont des tailles différentes." << std::endl;
+            exit(-1);
+        }
+
+        // Vérifie que les tableaux de gènes ne sont pas vides
+        if (genes.empty() || partner.genes.empty())
+        {
+            cout << "Erreur : Les tableaux de gènes sont vides." << std::endl;
+            exit(-1);
+        }
+        // creer les deux enfants
+        individu enfant_1(n);
+        individu enfant_2(n);
+
+        // Choisissez un point de croisement aléatoire
+        int pt_rand = unif_rand(n);
+        // copie des premieres valeurs de genes des enfants
+        for (int i = 0; i < pt_rand; ++i)
+        {
+            enfant_1.genes[i] = genes[i];
+            enfant_2.genes[i] = partner.genes[i];
+        }
+
+        // copie des autres valeurs des enfants
+        for (int i = pt_rand; i < n; ++i)
+        {
+            enfant_1.genes[i] = partner.genes[i];
+            enfant_2.genes[i] = genes[i];
+        }
+
+        return (enfant_1, enfant_2);
     }
 };
 
