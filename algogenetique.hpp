fd219c359c5c3f096c08d78ee29516ba34c6a2ae
pgatt06 <128541131+pgatt06@users.noreply.github.com>
2024-02-06 23:42:10 +0100

    ajout commentaires

--- algogenetique.hpp
+++ algogenetique.hpp
@@ -8,6 +8,10 @@ using namespace std;
 // fonction qui renvoie un nombre aléatoire entre k et n de manière uniforme
 int unif_rand(int k, int n);
 
+//=========================================================================================================================
+//                                              CLASSE INDIVIDU
+//=========================================================================================================================
+
 class Individu
 {
 private:
@@ -16,15 +20,42 @@ private:
     int n;
 
 public:
+    //=================================================================
+    //   fonctions virtuelles à définir dans la classe fille
+    //=================================================================
+
+    // fonction qui affiche le chemin
+    virtual void print(ostream &out) const = 0;
+
+    // fonction qui calcule la distance totale du chemin
     virtual double poids() const = 0;
+
+    //=================================================================
+    //         fonction propre à la classe Individu
+    //=================================================================
+
     Individu *flip();
     Individu *mutation();
     Individu *crossover(const Individu &partner);
 };
+
+//=========================================================================================================================
+//                                           CLASSE ENFANT AKA CHEMIN
+//=========================================================================================================================
 
 class Chemin : public Individu
 {
 private:
     vector<double> genes;
     double poid_chemin;
+};
+
+//=========================================================================================================================
+//                                                CLASSE POPULATION
+//=========================================================================================================================
+
+class population
+{
+public:
+    vector<Individu *> Individus;
 };
