c755be720c925b4d2f0c5f6345eaa91e732fa9cc
pgatt06 <128541131+pgatt06@users.noreply.github.com>
2024-02-06 10:37:56 +0100

    ajout fct

--- algogenetique.hpp
+++ algogenetique.hpp
@@ -21,11 +21,6 @@ private:
     vector<int> genes;
 
 public:
-    // constructeur avec une taille donnée
-    individu(int taille) : n(taille), genes(taille) {}
-    // constructeur avec des valeurs données
-    individu(const std::vector<int> &geneList) : n(geneList.size()), genes(geneList) {}
-
     //=========================================================================================================================
     //                         fonctions virtuelles propres à l'application considérée
     //=========================================================================================================================
@@ -37,13 +32,67 @@ public:
     //=========================================================================================================================
 
     // flip entre 4 gènes cf projet
-    individu flip(){};
+    individu *flip()
+    {
+        if (n <= 1)
+        {
+            cout << "mutation impossible car <=1";
+            exit(-1);
+        }
+
+        // deux indices alétoirement
+        int l = unif_rand(1, n - 1);
+        int k = unif_rand(0, n - 2);
+
+        // échange des valeurs
+        int gene_k = genes[k];
+        genes[k] = genes[k + 1];
+        genes[k + 1] = gene_k;
+
+        int gene_l = genes[l];
+        genes[k] = genes[l - 1];
+        genes[l - 1] = gene_l;
+    };
 
     // mutation entre deux gènes aléatoires
-    individu mutation(){};
+    individu *mutation(){};
 
     // création de deux enfants issus de 2 parents
-    individu crossover(const individu &ind2){};
+    individu *crossover(const individu &partner)
+    {
+        // Vérifie que les deux parents ont la même taille
+        if (n != partner.n)
+        {
+            cout << "Erreur : Les parents ont des tailles différentes." << std::endl;
+            exit(-1);
+        }
+        // Vérifie que les tableaux de gènes ne sont pas vides
+        if (genes.empty() || partner.genes.empty())
+        {
+            cout << "Erreur : Les tableaux de gènes sont vides." << std::endl;
+            exit(-1);
+        }
+        // creer les deux enfants
+        individu *enfant_1(n);
+        individu *enfant_2(n);
+
+        // Choisissez un point de croisement aléatoire
+        int pt_rand = unif_rand(n);
+        int pt_rand = unif_rand(0, n - 1);
+        // copie des premieres valeurs de genes des enfants
+        for (int i = 0; i < pt_rand; ++i)
+        {
+            cout << "mutation impossible car <=1";
+            exit(-1);
+        }
+        int l = unif_rand(n);
+        int k = unif_rand(n);
+        int l = unif_rand(0, n - 1);
+        int k = unif_rand(0, n - 1);
+        int gene_l = genes[l];
+        genes[l] = genes[k];
+        genes[k] = gene_l;
+    };
 };
 
 class population
