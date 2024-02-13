d2a2f6498b9e2be7597582640f4a88d5c76c8eb1
nbonetti <128616334+nbonetti@users.noreply.github.com>
2024-02-13 08:54:46 +0000

    fonction test

--- test.cpp
+++ test.cpp
@@ -1,10 +1,37 @@
 #include "algogenetique.hpp"
+#include "voyage.hpp"
+
 using namespace std;
 
 int main()
 {
     Individu *child1 = FactoryMethod(CheminType);
     child1->print(cout);
+    vector<string> cityNames = {"A", "B", "C"};
+    vector<Point> cityCoords = {Point(0, 0), Point(3, 4), Point(6, 8)};
+    Ville city(cityNames, cityCoords);
+    city.calcul_distance();
+    
+    cout << "Distances between cities:" << endl;
+    for (const auto& row : city.distances) {
+        for (double dist : row) {
+            cout << dist << "\t";
+        }
+        cout << endl;
+    }
+    vector<double> initialGenes = {0, 1, 2}; // Initial path: A -> B -> C
+    Chemin path(initialGenes, city);
 
+    cout << "Initial path: ";
+    path.print(cout);
+
+    cout << "Initial path weight: " << path.poids() << endl;
     return (0);
 }
+
+
+
+
+    
+
+  
