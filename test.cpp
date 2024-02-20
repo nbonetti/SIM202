98c14cfc4b012d1081dc233c6b2774db59490895
nbonetti <128616334+nbonetti@users.noreply.github.com>
2024-02-20 11:42:34 +0000

    test de la fonction mutation

--- test.cpp
+++ test.cpp
@@ -27,5 +27,17 @@ int main()
     path.print(cout);
 
     cout << "Initial path weight: " << path.poids() << endl;
+
+
+    //on a un chemin et on a calculé la distance de parcours 
+    //maintenant on va essayer d'appliquer mutation à l'indivu soit au chemin formé 
+    // Mutation du chemin
+    path.mutationPermutation(); // Vous pouvez choisir entre mutationPermutation() et mutationAleatoire() selon votre besoin
+    // Affichage du chemin après mutation et recalcul de la distance parcourue
+    cout << "Path after mutation: ";
+    path.print(cout);
+    cout << "Path weight after mutation: " << path.poids() << endl;
+
     return (0);
 }
+
