d32d0b08c193e4ac00c0aa7a893907482ebb1b78
pgatt06 <128541131+pgatt06@users.noreply.github.com>
2024-02-27 12:02:53 +0100

    correction shuffle

--- algogenetique.cpp
+++ algogenetique.cpp
@@ -124,9 +124,10 @@ Population reproduction(const Population parents, Individu *(*pointeur_FactoryMe
     Population parentsMelanges = parents;
 
     // Mélange l'ordre des individus dans la population parentale
-    // std::random_device rd;
-    // std::mt19937 g(rd());
-    // std::shuffle(parentsMelanges.getIndividu(0), parentsMelanges.getIndividu(parents.getTaillePopulation()), g);
+    std::random_device rd;
+    std::mt19937 g(rd());
+    vector<Individu *> individus = parentsMelanges.getIndividus();
+    std::shuffle(individus.begin(), individus.end(), g);
 
     Population enfants; // La population des descendants
 
