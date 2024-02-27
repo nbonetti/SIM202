7a076762bb55f515af17b08eaf849ceb7a55df67
nbonetti <128616334+nbonetti@users.noreply.github.com>
2024-02-27 10:12:13 +0000

    test reproduction

--- test.cpp
+++ test.cpp
@@ -21,7 +21,7 @@ int main()
         }
         cout << endl;
     }
-    vector<double> initialGenes = {0, 1, 2,3,4}; // Initial path: A -> B -> C
+    vector<double> initialGenes = {0, 1, 2,3,4}; // Initial path: A -> B -> C->D->E
     Chemin path(initialGenes, city);
 
     cout << "Initial path: ";
@@ -33,11 +33,40 @@ int main()
     //on a un chemin et on a calculé la distance de parcours 
     //maintenant on va essayer d'appliquer mutation à l'indivu soit au chemin formé 
     // Mutation du chemin
-    path.mutationPermutation(); // Vous pouvez choisir entre mutationPermutation() et mutationAleatoire() selon votre besoin
+    //path.mutationPermutation(); // Vous pouvez choisir entre mutationPermutation() et mutationAleatoire() selon votre besoin
     // Affichage du chemin après mutation et recalcul de la distance parcourue
-    cout << "Path after mutation: ";
+    /*cout << "Path after mutation: ";
     path.print(cout);
-    cout << "Path weight after mutation: " << path.poids() << endl;
+    cout << "Path weight after mutation: " << path.poids() << endl;*/
+
+
+
+    //-------------------------test de reproduction---------------------------------------//
+
+
+    // Création d'une population de chemins
+    vector<Individu *> populationInitiale;
+    populationInitiale.push_back(&path); // Ajout du chemin initial à la population
+
+    Population population(populationInitiale);
+
+    // Test de la fonction de reproduction
+    Population enfants = reproduction(population, FactoryMethod);
+
+    // Affichage des enfants générés
+    cout << "Enfants générés: " << endl;
+    for (int i = 0; i < enfants.getTaillePopulation(); ++i)
+    {
+        Chemin *enfant = dynamic_cast<Chemin *>(enfants.getIndividu(i));
+        if (enfant)
+        {
+            cout << "Enfant " << i + 1 << ": ";
+            enfant->print(cout);
+            cout << "Poids de l'enfant " << i + 1 << ": " << enfant->poids() << endl;
+        }
+    }
+
+    
 
     return (0);
 }
