fa73de777996a56849e7ca76d74c955cce561c15
nbonetti <128616334+nbonetti@users.noreply.github.com>
2024-03-05 08:26:18 +0000

    selection

--- selection.hpp
+++ selection.hpp
@@ -101,4 +101,104 @@ Population selection_rang(Population &Pop_initiale)
     return P_finale;
 }
 
+
+//sélection par eugénisme
+
+population select_eugenisme(population& P, int p) 
+{
+    // Trier par la fonction d'adaptation (le plus faible est le mieux)
+    sort(P.individus.begin(), P.individus.end(), [](const individu* a, const individu* b) {
+        return a->coute() < b->coute();
+    });
+    
+    // Séléctionner le meilleur pourcentage p% d'individus reproducteurs
+    int n = P.individus.size();
+    int m = (int) ((double) p)/100.0 * (double) n;
+    population Q(m);
+
+    for (int i = 0; i < m; i++) {
+        Q.individus[i] = P.individus[i]->clone();
+    }
+    
+    return Q;
+}
+
+//Sélection par roulette
+
+population select_roulette(population &P, int p) {
+    srand(time(0));
+    population P_select(p);
+    vector<double> vdist;
+    vector<double> vdist_temp;
+    double S=0;
+    vector<int> rang;
+
+    //Remplissage des vecteurs des longueurs des individus et de leur rang, et calcul de la somme des fonctions d'adaptation
+    for (int i;i<P.individus.size();i++) {
+        vdist.push_back(P.individus[i]->coute());
+        S=S+P.individus[i]->coute();
+        rang.push_back(i);
+        }
+
+    //Sélection par roulette avec la fonction d'adaptation 
+
+    for (int j=0;j<p;j++) {
+        int a=0;
+        double r=S*((double) rand() / (RAND_MAX));
+        double S_temp=0;
+        while (S_temp<r) {
+            S_temp= S_temp + vdist[rang[a]];
+            a=a+1;
+        }
+        P_select.individus.push_back(P.individus[rang[a]]);
+        rang.erase(rang.begin()+a);
+    }
+    return P_select;
+}
+
+//Sélection par rang
+
+population select_rang(population &P, int p) {
+    srand(time(0));
+    population P_select(p);
+    vector<double> vdist;
+    vector<double> vdist_temp;
+    int S=0;
+    vector<int> rang;
+
+    //Remplissage de vecteurs de fonctions d'adaptation puis tri
+
+    for (int i;i<P.individus.size();i++) {
+        vdist.push_back(P.individus[i]->coute());
+        vdist_temp.push_back(P.individus[i]->coute());
+        S=S+i;
+        rang.push_back(i);
+        }
+    sort(vdist.begin(), vdist.end());
+
+    //Tri des individus du plus mauvais au meilleur
+    int index=0;
+    for (int j;j<P.individus.size();j++) {
+        index = findIndex(vdist_temp, vdist[j]);
+        P.individus[P.individus.size()-1-j]=P.individus[index];
+        }
+
+    //Sélection par roulette avec la fonction de rang
+    for (int k=0;k<p;k++) {
+        int l= rand() % S;
+        int S_temp=0;
+        int a=0;
+        while (S_temp<l) {
+            S_temp=S_temp+rang[a];
+            a=a+1;
+        }
+        P_select.individus.push_back(P.individus[rang[a]]);
+        rang.erase(rang.begin()+a);
+
+    }
+    return P_select;
+
+}
+
+
 #endif
