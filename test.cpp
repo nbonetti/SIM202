71f01a617bf8cce86b9549f03bff48cd881ed820
nbonetti <128616334+nbonetti@users.noreply.github.com>
2024-03-06 17:36:49 +0000

    test

--- test.cpp
+++ test.cpp
@@ -7,9 +7,9 @@ using namespace std;
 
 int main()
 {
+    //définiton des villes à parcourir et calcul des distances
     vector<int> cityNames = {0,1,2,3,4};
     vector<Point> cityCoords = {Point(0, 0), Point(3, 4), Point(6, 8), Point(10, 5), Point(12, 2)};
-
     Ville city(cityNames, cityCoords);
     city.calcul_distance();
 
@@ -22,12 +22,23 @@ int main()
         }
         cout << endl;
     }
-    vector<double> initialGenes = {0, 1, 2, 3, 4}; // Initial path: A -> B -> C->D->E
-    Chemin path(initialGenes, city);
+    // Génération de plusieurs chemins initiaux
+    vector<vector<double>>initialPaths = {
+        {0, 1, 2, 3, 4},  // Chemin 1
+        {0, 2, 1, 3, 4},  // Chemin 2
+        {0, 4, 3, 2, 1}   // Chemin 3
+    };
+    //cre le chemin initial à partir de la liste des chemins
+    vector<double> intGenes;
+    intGenes.reserve(initialPaths[0].size());
+    for (double gene : initialPaths[0])
+    {
+        intGenes.push_back(static_cast<int>(gene));
+    }   
+    Chemin path(intGenes, city);
 
     cout << "Initial path: ";
     path.print(cout);
-
     cout << "Initial path weight: " << path.poids() << endl;
 
     // on a un chemin et on a calculé la distance de parcours
@@ -43,18 +54,23 @@ int main()
 
     // Création d'une population de chemins
     vector<Individu *> populationInitiale;
-    populationInitiale.push_back(&path); // Ajout du chemin initial à la population
-
+    //populationInitiale.push_back(&path); // Ajout du chemin initial à la population
+    for (const auto &genes : initialPaths)
+    {
+        populationInitiale.push_back(new Chemin(genes, city));
+    }
     Population population(populationInitiale);
 
     cout << "la pop est \n";
     population.print(cout);
+    cout<<endl;
     // Test de la fonction de sélection
-    Population selection = selection_eugenisme(population, 1);
-
+    //Population selection = selection_eugenisme(population, 1);
+    Population selection = selection_rang(population);
     // Affichage des individus sélectionnés
     cout << "Individus sélectionnés: " << endl;
     selection.print(cout);
+    cout<<endl;
     // for (int i = 0; i < selection.getTaillePopulation(); ++i)
     // {
     //     Chemin *individu = dynamic_cast<Chemin *>(selection.getIndividu(i));
@@ -72,7 +88,8 @@ int main()
     Population enfants = reproduction(selection, FactoryMethod);
 
     cout << "affiche les enfants \n";
-    // enfants.print(cout);
+    enfants.print(cout);
+    cout<<endl;
 
     return (0);
 }
