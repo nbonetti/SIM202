81ef2fa04e7c1f4cb58d076020a07c263461c808
nbonetti <128616334+nbonetti@users.noreply.github.com>
2024-03-06 17:44:52 +0000

    test

--- test.cpp
+++ test.cpp
@@ -7,89 +7,43 @@ using namespace std;
 
 int main()
 {
-    //définiton des villes à parcourir et calcul des distances
-    vector<int> cityNames = {0,1,2,3,4};
-    vector<Point> cityCoords = {Point(0, 0), Point(3, 4), Point(6, 8), Point(10, 5), Point(12, 2)};
+    // Création des données des villes à parcourir
+    vector<int> cityNames = {0, 1, 2, 3, 4};
+    vector<Point> cityCoords = {Point(0, 0), Point(3, 4), Point(6, 8), Point(10, 5), Point(12, 2)};//coordonnées de chaque ville
     Ville city(cityNames, cityCoords);
     city.calcul_distance();
 
-    cout << "Distances between cities:" << endl;
-    for (const auto &row : city.distances)
-    {
-        for (double dist : row)
-        {
-            cout << dist << "\t";
-        }
-        cout << endl;
-    }
-    // Génération de plusieurs chemins initiaux
-    vector<vector<double>>initialPaths = {
-        {0, 1, 2, 3, 4},  // Chemin 1
-        {0, 2, 1, 3, 4},  // Chemin 2
-        {0, 4, 3, 2, 1}   // Chemin 3
+    // Création de plusieurs chemins initiaux
+    vector<vector<double>> paths = {
+        {0, 1, 2, 3, 4}, // Chemin 1
+        {0, 2, 4, 1, 3}, // Chemin 2
+        {0, 3, 2, 1, 4}  // Chemin 3
     };
-    //cre le chemin initial à partir de la liste des chemins
-    vector<double> intGenes;
-    intGenes.reserve(initialPaths[0].size());
-    for (double gene : initialPaths[0])
-    {
-        intGenes.push_back(static_cast<int>(gene));
-    }   
-    Chemin path(intGenes, city);
 
-    cout << "Initial path: ";
-    path.print(cout);
-    cout << "Initial path weight: " << path.poids() << endl;
-
-    // on a un chemin et on a calculé la distance de parcours
-    // maintenant on va essayer d'appliquer mutation à l'indivu soit au chemin formé
-    //  Mutation du chemin
-    path.mutationPermutation(); // Vous pouvez choisir entre mutationPermutation() et mutationAleatoire() selon votre besoin
-    //  Affichage du chemin après mutation et recalcul de la distance parcourue
-    cout << "Path after mutation: ";
-    path.print(cout);
-    cout << "Path weight after mutation: " << path.poids() << endl;
-
-    //-------------------------test de sélection ---------------------------------------//
-
-    // Création d'une population de chemins
+    // Création des individus pour chaque chemin initial
     vector<Individu *> populationInitiale;
-    //populationInitiale.push_back(&path); // Ajout du chemin initial à la population
-    for (const auto &genes : initialPaths)
+    for (auto &path : paths)
     {
-        populationInitiale.push_back(new Chemin(genes, city));
+        populationInitiale.push_back(new Chemin(path, city));
     }
+
+    // Création de la population initiale
     Population population(populationInitiale);
 
-    cout << "la pop est \n";
+    cout << "Population initiale:" << endl;
     population.print(cout);
-    cout<<endl;
-    // Test de la fonction de sélection
-    //Population selection = selection_eugenisme(population, 1);
-    Population selection = selection_rang(population);
-    // Affichage des individus sélectionnés
-    cout << "Individus sélectionnés: " << endl;
+
+    /*// Sélection des individus pour la reproduction
+    Population selection = select_roulette(population, 2);
+
+    cout << "\nIndividus sélectionnés pour la reproduction:" << endl;
     selection.print(cout);
-    cout<<endl;
-    // for (int i = 0; i < selection.getTaillePopulation(); ++i)
-    // {
-    //     Chemin *individu = dynamic_cast<Chemin *>(selection.getIndividu(i));
-    //     if (individu)
-    //     {
-    //         cout << "Individu " << i + 1 << ": ";
-    //         individu->print(cout);
-    //         cout << "Poids de l'individu " << i + 1 << ": " << individu->poids() << endl;
-    //     }
-    // }
 
-    //-------------------------test de reproduction---------------------------------------//
-
-    // Test de la fonction de reproduction
+    // Reproduction des individus sélectionnés
     Population enfants = reproduction(selection, FactoryMethod);
 
-    cout << "affiche les enfants \n";
-    enfants.print(cout);
-    cout<<endl;
+    cout << "\nEnfants après reproduction:" << endl;
+    enfants.print(cout);*/
 
-    return (0);
+    return 0;
 }
