d24d460fc621da1cdcf4f034c11f2a9e61b254e2
pgatt06 <128541131+pgatt06@users.noreply.github.com>
2024-03-07 15:57:55 +0100

    ajout algo genetic

--- main.cpp
+++ main.cpp
@@ -15,33 +15,119 @@
 #include "csv.hpp"
 using namespace std;
 
+#include <cmath>
+#include <cstdlib>
+#include <vector>
+#include <random>
+#include <ctime>
+#include <list>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+#include "algogenetique.hpp"
+#include "voyage.hpp"
+#include "Factory.hpp"
+#include "selection.hpp"
+using namespace std;
+
+Population algogenetique(Population &Pop_initiale, int nb_iter, int nb_reproducteurs)
+{
+
+    //====================================================================================================================================
+    //                          initialisation
+    //====================================================================================================================================
+
+    int k = 0;
+    // nb de parents nécessaires pour que la population après reproduction et sélection garde la même taille intiale
+    int nb_parents_conservés = Pop_initiale.getTaillePopulation() - nb_reproducteurs;
+
+    Population population_parents = Pop_initiale;
+
+    //====================================================================================================================================
+    //                          iteration jusqu'à fin (critère d'arret )
+    //====================================================================================================================================
+    while (k < nb_iter)
+    {
+        //====================================================================================================================================
+        //                          SELECTION DES REPRODUCTEURS
+        //====================================================================================================================================
+
+        // proba de gagner lors de la selection par tournoi
+        double proba_tournoi;
+
+        //  Population parents_selectionnés = selection_eugenisme(population_parents, nb_reproducteurs);
+        //  Population parents_selectionnés = selection_roulette(population_parents, nb_reproducteurs);
+        Population parents_selectionnés = selection_rang(population_parents, nb_reproducteurs);
+        // Population parents_selectionnés = selection_tournoi(population_parents, nb_reproducteurs, proba_tournoi);
+
+        //====================================================================================================================================
+        //                          REPRODUCTION DES ENFANTS + MUTATION
+        //====================================================================================================================================
+        // il faut choisir la mutation appliquée aux enfants
+        // enfants des parents issus de la sélection
+        Population enfants = reproduction(parents_selectionnés, FactoryMethod);
+
+        // mutation des enfants
+        for (int i = 0; i < nb_reproducteurs; i++)
+        {
+            enfants.getIndividu(i)->mutationAleatoire();
+            // enfants.getIndividu(i)->mutationPermutation();
+        }
+
+        //====================================================================================================================================
+        //                          COMPLEMENT DE LA POPULATION ENFANTS POUR CONSERVER LE NB D'INDIVIDUS
+        //====================================================================================================================================
+
+        // à définir si sélection par tournoi
+        double proba_tournoi_conservés;
+        // sélection des parents que l'on va conservés pour la prochaine génération
+        // Population parents_conservés = selection_eugenisme(population_parents, nb_reproducteurs);
+        // Population parents_selectionnés = selection_roulette(population_parents, nb_parents_conservés);
+        Population parents_conservés = selection_rang(population_parents, nb_parents_conservés);
+        // Population parents_conservés = selection_tournoi(population_parents, nb_parents_conservés, proba_tournoi_conservés);
+
+        enfants = enfants + parents_conservés;
+
+        //====================================================================================================================================
+        //                         ITERATEURS
+        //====================================================================================================================================
+
+        population_parents = enfants;
+        k = k + 1;
+    };
+
+    // la population finale est
+    Population pop_finale = population_parents;
+
+    return pop_finale;
+};
 
 int main()
 
-{/*=========================================================================================================
-//                                             LECTURE DU FICHIER 
-//==========================================================================================================
-*/
-    vector<vector<double>> content= csv_reader("Villes_csv");//on récupère le fichier 
+{                                                              /*=========================================================================================================
+                                                              //                                             LECTURE DU FICHIER
+                                                              //==========================================================================================================
+                                                              */
+    vector<vector<double>> content = csv_reader("Villes_csv"); // on récupère le fichier
 
-//========================================================================================
-//                                      DEBUT 
-//========================================================================================
+    //========================================================================================
+    //                                      DEBUT
+    //========================================================================================
 
+    vector<int> citynumber; // index des villes dans le fichier
+    vector<Point> coord;    // coordonées (x,y)
 
-    vector<int> citynumber;//index des villes dans le fichier 
-    vector<Point> coord;//coordonées (x,y)
-    
     for (const auto &row : content)
     {
-        cout<<row[0];
-        citynumber.push_back(row[0]); // Première colonne : indices
+        cout << row[0];
+        citynumber.push_back(row[0]);           // Première colonne : indices
         coord.push_back(Point(row[1], row[2])); // Deuxième et troisième colonnes : coordonnées x et y
     }
-    //création de l'objet ville 
-    Ville city(citynumber,coord);
+    // création de l'objet ville
+    Ville city(citynumber, coord);
     city.calcul_distance();
-    city.print(cout); 
+    city.print(cout);
     cout << "Distances between cities:" << endl;
     for (const auto &row : city.distances)
     {
@@ -56,28 +142,28 @@ int main()
 
 
 
-    
+
 
 
 
 //==============================================================================
-//                                  DEBUT 
+//                                  DEBUT
 //==============================================================================
-    Population paths;//population initaile avec tou sles chemins possibles avec le fichier de départ 
+    Population paths;//population initaile avec tou sles chemins possibles avec le fichier de départ
     int nombre_chemins; //choisir le nombre de chemins que l'on veut dans notre porpulation
     int i = 0;
     int k = 0;
-//faire une fonction qui génère la population totale avec tous les chemins possibles avec le fichier de départ 
+//faire une fonction qui génère la population totale avec tous les chemins possibles avec le fichier de départ
     generateur_chemins(nombre_chemins,city,paths);
     paths.print(cout);
 
 
-    Population enfants; 
-    int p; //choisir nous mêmes 
-    //test d'arrêt 
+    Population enfants;
+    int p; //choisir nous mêmes
+    //test d'arrêt
     while (k<=5 && i<=100)
     {
-        //SELECTION DE LA POPULATION  
+        //SELECTION DE LA POPULATION
         Population selection=selection_rang(paths);
         //Population selection=select_roulette(paths,p);
         //Population selection=select_eugenisme(paths,p);
@@ -89,14 +175,14 @@ int main()
         Individu* I=paths.getIndividu(0);
         double minimum= I->poids();
 
-        //HYBRIDATION -REPRODUCTION 
+        //HYBRIDATION -REPRODUCTION
         // Reproduction des individus sélectionnés
         enfants = reproduction(selection, FactoryMethod);
         cout << "\nEnfants après reproduction:" << endl;
         enfants.print(cout);
 
 
-        //MUTATION 
+        //MUTATION
         for (int i = 0; i < enfants.getTaillePopulation(); ++i)
         {
             enfants.getIndividu(i)->mutationPermutation();
@@ -105,15 +191,12 @@ int main()
         cout << "\nEnfants après mutation:" << endl;
         enfants.print(cout);
 
-        
-        
+
+
 
 
 
     }*/
 
-    return 0; 
+    return 0;
 }
-
-
-
