7f9e1a28aee809fcb4e201c368550b27f6532ef1
nbonetti <128616334+nbonetti@users.noreply.github.com>
2024-03-07 12:20:35 +0000

    main

--- main.cpp
+++ main.cpp
@@ -15,21 +15,34 @@
 using namespace std;
 
 
+int main()
 
-/*=========================================================================================================
+{/*=========================================================================================================
 //                                             LECTURE DU FICHIER 
 //==========================================================================================================
 */
-vector<vector<double>> content= csv_reader("Villes_csv");//on récupère le fichier 
+    vector<vector<double>> content= csv_reader("Villes_csv");//on récupère le fichier 
 
 
-vector<int> citynumber;
-vector<Point> coord;
-vector<double> vmin;
-int i = 0;
-int k = 0;
+    vector<int> citynumber;
+    vector<Point> coord;
+    vector<double> vmin;
+    int i = 0;
+    int k = 0;
 
-Ville city(citynumber,coord);
+    Ville city(citynumber,coord);
+    Population paths;//population initaile avec tou sles chemins possibles avec le fichier de départ 
+    int nombre_chemins; //choisir le nombre de chemins que l'on veut dans notre porpulation
+
+//faire une fonction qui génère la population totale avec tous les chemins possibles avec le fichier de départ 
+//generateur_chemins(nombre_chemins,city,paths);
+    paths.print(cout);
+
+
+    Population enfants; 
+    
+
+}
 
 
 
